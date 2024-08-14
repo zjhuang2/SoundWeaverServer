1fb5c3305ff8213c160c7ff28b4f562130cb480a
Jeremy Huang <jeremy.zq.huang@gmail.com>
2024-08-14 15:42:13 -0400

    Initial Commit

---
+++ SoundWeaverServer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundWeaverServer
+//
+//  Created by Jeremy Huang on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundWeaverServer/SoundWeaverServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundWeaverServerApp.swift
+//  SoundWeaverServer
+//
+//  Created by Jeremy Huang on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundWeaverServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
